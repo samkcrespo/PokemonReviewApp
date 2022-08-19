38cad9c651fb285e2c4e128b17ec0652262f9a7c
samkcrespo <samkcrespo@gmail.com>
2022-08-19 16:24:30 -0700

    Migrations completed

---
+++ PokemonReviewApp/Models/PokemonOwner.cs
@@ -0,0 +1,15 @@
+﻿namespace PokemonReviewApp.Models
+{
+
+    //Join Table
+    public class PokemonOwner
+    {
+
+        public int PokemonId { get; set; }
+        public int OwnerId { get; set; }
+
+        public Pokemon Pokemon { get; set; }
+        public Owner Owner { get; set; }
+
+    }
+}
